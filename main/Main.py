480422d069463648e35af3f43d6f6bbb0a7f69dd
meaad-farag <128365016+meaad-farag@users.noreply.github.com>
2023-04-24 10:43:30 +0300

    Update Main.py
    Connect Flask to a Database

--- main/Main.py
+++ main/Main.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 # TODO: make the app config to connect it to database
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-db.create_all()
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db.create_all()
 
 @app.route('/')
 def index():
