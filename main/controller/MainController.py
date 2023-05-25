592f2df512d3a8939910fd3501c8605ba213e8df
ibrahimAlroubh <ibrahrpbh@gmail.com>
2023-05-25 12:03:46 -0700

    update and restruct the model fill

--- main/controller/MainController.py
+++ main/controller/MainController.py
@@ -1,5 +1,7 @@
 from flask import jsonify
 from main.main import app
+from main.model import Modles
+import random
 
 
 @app.route('/api', methods=['GET'])
@@ -8,3 +10,15 @@ def api():
         'message': 'Hello, World!'
     }
     return jsonify(data)
+
+
+@app.route('/marketChatbot/api/v1/randomProduct', methods=['GET'])
+def random_products():
+    products = get_three_random_products()
+    return jsonify(products)
+
+
+def get_three_random_products():
+    all_products = Modles.Product.query.all()
+    random_products = random.sample(all_products, 3)
+    return random_products
