b77bc643786460618f1151f7120cf1d10e0e1d89
ibrahimAlroubh <ibrahrpbh@gmail.com>
2023-05-25 07:50:37 -0700

    update and restruct the model fill

--- main/Main.py
+++ main/Main.py
@@ -1,38 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask
 from main.model.Modles import db
-from main.model.Modles import Store
 import os
 
 
 def create_app():
     app = Flask(__name__)
-    # set DATABASE_URL=postgresql://smchatbotdatabase_user:YLSGBHn1SnRKmRKRpiY2ZImMjdb0CciZ@dpg-chn735vdvk4n43a6131g-a.oregon-postgres.render.com/smchatbotdatabase
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
 
 
-app = create_app()
-
-
-@app.route('/', methods=['GET'])
-def main():
-    return "hi welcome :)"
-
-
-@app.route('/test', methods=['GET'])
-def test():
-    print("it is work at lest")
-    try:
-        s = Store.query.filter_by(name='ss').first()
-        return s.name
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
 if __name__ == '__main__':
-    # Create the database tables
     app = create_app()
     with app.app_context():
         db.create_all()
--- main/controller/MainController.py
+++ main/controller/MainController.py
@@ -1,18 +1,12 @@
-from flask import request
-from main.Main import app
-from main.model import Modles
+from flask import request, jsonify
+from run import app
 
 
-@app.route("/chop/api/v1/message", methods=['POST'])
-def receive_message():
-    message = request.get_json()  # Extract the message from the request JSON
+@app.route('/api', methods=['GET'])
+def api():
+    data = {
+        'message': 'Hello, World!'
+    }
+    return jsonify(data)
 
-    print("Received message:", message)  # Print the message
-
-    return {"success": True}  # Return a response indicating success
-
-
-@app.route("/chop/api/v1/randomProduct", methods=['POST'])
-def random_product():
-    message = request.get_json()
 
