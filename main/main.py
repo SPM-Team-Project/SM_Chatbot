5eba8a504f3e508ce4d54cbf3769d74b5ac35e30
ibrahimAlroubh <ibrahrpbh@gmail.com>
2023-05-25 08:33:05 -0700

    update and restruct the model fill

--- main/main.py
+++ main/main.py
@@ -7,4 +7,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-from controller import MainController
+
+from main.controller import MainController
