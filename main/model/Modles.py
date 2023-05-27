6b4750ad39fd0cc839b5f23dcd42cd2d2999c665
ibrahimAlroubh <ibrahrpbh@gmail.com>
2023-05-27 07:37:26 -0700

    update and restruct the model fill

--- main/model/Modles.py
+++ main/model/Modles.py
@@ -2,76 +2,81 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-order_product = db.Table('order_product',
-    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
-    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
-)
-
-
 class Customer(db.Model):
-    __tablename__ = 'cusomer'
+    __tablename__ = 'customer'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(55))
-    address = db.Column(db.String(255))
-    email = db.Column(db.String(55))
-    password = db.Column(db.String(55))
-    phone = db.Column(db.String(55))
-    store_name = db.Column(db.String(60), db.ForeignKey("e_store.name"), nullable=False)
+    c_id = db.Column(db.Integer, primary_key=True)
+    c_name = db.Column(db.String(60), nullable=False)
+    c_address = db.Column(db.String(60))
+    c_email = db.Column(db.String(60), nullable=False)
+    c_phone = db.Column(db.String(60))
+    e_storeName = db.Column(db.String(250), db.ForeignKey("e_store.name"))
     orders = db.relationship('Order', backref='customer', lazy=True)
 
-    def __init__(self, cid, name, address, email, password, phone):
-        self.id = cid
-        self.name = name
-        self.address = address
-        self.email = email
-        self.password = password
-        self.phone = phone
+    def __init__(self, c_id, c_name, c_address, c_email, c_password, c_phone, e_storeName):
+        self.c_id = c_id
+        self.c_name = c_name
+        self.c_address = c_address
+        self.c_email = c_email
+        self.c_password = c_password
+        self.c_phone = c_phone
+        self.e_storeName = e_storeName
 
 
-class Product(db.Model):
-    __tablename__ = 'product'
-
-    id = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String(255))
-    stock = db.Column(db.Integer)
-    price = db.Column(db.Double)
-    size = db.Column(db.String(5))
-    color = db.Column(db.String(55))
-    store_name = db.Column(db.String(60), db.ForeignKey("e_store.name"), nullable=False)
-    orders = db.relationship('Order', secondary=order_product, backref='products', lazy=True)
-
-    def __init__(self, pid, category, stock, price, size, color):
-        self.id = pid
-        self.category = category
-        self.stock = stock
-        self.price = price
-        self.size = size
-        self.color = color
-
-
-class Store(db.Model):
+class EStore(db.Model):
     __tablename__ = 'e_store'
 
     name = db.Column(db.String(60), primary_key=True)
     address = db.Column(db.String(60))
     phone_no = db.Column(db.String(60))
-    customers = db.relationship('Customer', backref='customer', lazy=True)
-    products = db.relationship('Product', backref='product', lazy=True)
+    products = db.relationship('Product', backref='e_store', lazy=True)
 
-    def __init__(self):
-        pass
+    def __init__(self, name, address, phone_no):
+        self.name = name
+        self.address = address
+        self.phone_no = phone_no
 
-    def __repr__(self):
-        return f'<Store {self.name}>'
+
+class Product(db.Model):
+    __tablename__ = 'product'
+
+    p_id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(60))
+    stock = db.Column(db.String(60))
+    price = db.Column(db.Integer)
+    size = db.Column(db.String(3))
+    color = db.Column(db.String(60))
+    p_code = db.Column(db.Integer)
+    e_storeName = db.Column(db.String(250), db.ForeignKey("e_store.name"))
+
+    def __init__(self, p_id, category, stock, price, size, color, p_code, e_storeName):
+        self.p_id = p_id
+        self.category = category
+        self.stock = stock
+        self.price = price
+        self.size = size
+        self.color = color
+        self.p_code = p_code
+        self.e_storeName = e_storeName
 
 
 class Order(db.Model):
-    __tablename__ = 'order'
+    __tablename__ = 'orders'
 
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(20))
-    shipping_address = db.Column(db.String(255))
-    total = db.Column(db.Integer)
-    state = db.Column(db.String(10))
-    customer_id = db.Column(db.Integer, db.ForeignKey('cusomer.id'), nullable=False)
+    o_date = db.Column(db.Date)
+    o_shippingAddress = db.Column(db.String(250))
+    o_state = db.Column(db.String(250))
+    o_total = db.Column(db.Float)
+    o_number = db.Column(db.Integer, nullable=False)
+    cusID = db.Column(db.Integer, db.ForeignKey('customer.c_id'), nullable=False)
+    proID = db.Column(db.Integer, db.ForeignKey('product.p_id'), nullable=False)
+
+    def __init__(self, o_date, o_shippingAddress, o_state, o_total, o_number, cusID, proID):
+        self.o_date = o_date
+        self.o_shippingAddress = o_shippingAddress
+        self.o_state = o_state
+        self.o_total = o_total
+        self.o_number = o_number
+        self.cusID = cusID
+        self.proID = proID
