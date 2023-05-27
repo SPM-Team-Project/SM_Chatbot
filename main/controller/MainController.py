1b7fddae2412fc7f93ba9d7cbc4b4f04f83675d8
ibrahimAlroubh <ibrahrpbh@gmail.com>
2023-05-27 13:51:40 -0700

    update and restruct the model fill

--- main/controller/MainController.py
+++ main/controller/MainController.py
@@ -1,12 +1,10 @@
-from flask import jsonify
+from flask import jsonify, request
 from main.main import app
-from main.model import Modles
-import random
+from main.servec import MainServec
 
 
-@app.route('/api', methods=['GET'])
+@app.route('/', methods=['GET'])
 def api():
-    print("test")
     data = {
         'message': 'Hello, World!'
     }
@@ -15,26 +13,12 @@ def api():
 
 @app.route('/marketChatbot/api/v1/randomProduct', methods=['GET'])
 def random_products():
-    print("main test")
-    products = get_three_random_products()
+    products = MainServec.get_three_random_products()
     return jsonify(products)
 
 
-def get_three_random_products():
-    print("test1")
-    all_products = Modles.Product.query.all()
-    print("test2")
-    random_products = random.sample(all_products, 3)
-    product_list = []
-    for index, product in enumerate(random_products, start=1):
-        product_data = {
-            f'p{index}': {
-                'category': product.category,
-                'price': product.price,
-                'size': product.size,
-                'color': product.color
-            }
-        }
-        product_list.append(product_data)
-    print("test3")
-    return product_list
+@app.route('/marketChatbot/api/v1', methods=['POST'])
+def get_product_by_code():
+    code = request.args.get('code')
+    product = MainServec.get_product_by_code(code)
+    return jsonify(product)
--- main/servec/MainServec.py
+++ main/servec/MainServec.py
@@ -1,3 +1,32 @@
+from main.model import Modles
+import random
 
-def message_handel(message):
-    pass
+
+def get_three_random_products():
+    all_products = Modles.Product.query.all()
+    random_products = random.sample(all_products, 3)
+    product_list = []
+    for index, product in enumerate(random_products, start=1):
+        product_data = {
+            f'p{index}': {
+                'category': product.category,
+                'price': product.price,
+                'size': product.size,
+                'color': product.color
+            }
+        }
+        product_list.append(product_data)
+    return product_list
+
+
+def get_product_by_code(code):
+    query_result = Modles.Product.query.filter_by(p_code=code).first()
+    product = {
+        'category': query_result.category,
+        'stock': query_result.stock,
+        'price': query_result.price,
+        'size': query_result.size,
+        'code': query_result.code,
+        'color': query_result.color
+    }
+    return product
